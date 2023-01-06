ad7a5580ae5a57abdbd41bb2e2d5bdb4887d2e46
Frédéric Gaufichon <fgaufichon@hotmail.fr>
2023-01-06 12:41:29 +0100

    begin fetch and functions for the game

--- dist/main.js
+++ dist/main.js
@@ -1,2 +1,21 @@
 "use strict";
+let Pays = [];
+let unPays, {} = {};
+fetch("https://restcountries.com/v3.1/all").then(function (response) {
+    var contentType = response.headers.get("content-type");
+    if (contentType && contentType.indexOf("application/json") !== -1) {
+        return response.json().then(function (json) {
+            startApplication(json);
+        });
+    }
+    else {
+        console.log("Oops, nous n'avons pas du JSON!");
+    }
+});
+function startApplication(e) {
+    Pays = [e];
+    console.log(Pays);
+}
+var rand = Math.random() * Pays.length | 0;
+let chosenCountry = Pays[rand];
 //# sourceMappingURL=main.js.map
